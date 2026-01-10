8b86dfc7baaea0020e91932794462bff73cebd52
PADALA TEJASAI <padalatejasai@gmail.com>
2026-01-10 07:26:55 -0800

    Update TB.v

--- DAY_10/TB.v
+++ DAY_10/TB.v
@@ -8,3 +8,9 @@ module tb();
       #5 a=0;b=1;
       #5 a=1;b=0;
       #5 a=1;b=1;
+    end
+  initial
+    begin
+      $monitor("a=%0b,b=%0b,o1=%0b,o2=%0b,o3=%0b,o4=%0b",a,b,o1,o2,o3,o4);
+    end 
+endmodule
