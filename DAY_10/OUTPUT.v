ba01d0ff96bf9473535fe389fa39ec597db42513
PADALA TEJASAI <padalatejasai@gmail.com>
2026-01-10 07:26:14 -0800

    Create OUTPUT.v

---
+++ DAY_10/OUTPUT.v
@@ -0,0 +1,5 @@
+KERNEL: a=x,b=x,o1=x,o2=x,o3=x,o4=x
+# KERNEL: a=0,b=0,o1=1,o2=0,o3=0,o4=0
+# KERNEL: a=0,b=1,o1=0,o2=1,o3=0,o4=0
+# KERNEL: a=1,b=0,o1=0,o2=0,o3=1,o4=0
+# KERNEL: a=1,b=1,o1=0,o2=0,o3=0,o4=1
