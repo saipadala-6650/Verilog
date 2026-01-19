8d31cea8d78c735592fb7fab49e6a94527f63ad6
PADALA TEJASAI <padalatejasai@gmail.com>
2026-01-19 07:25:46 -0800

    Create Output.v

---
+++ DAY_15/Output.v
@@ -0,0 +1,9 @@
+a=x,b=x,c=x,sum=x,carry=x
+# KERNEL: a=0,b=0,c=0,sum=0,carry=0
+# KERNEL: a=0,b=0,c=1,sum=1,carry=0
+# KERNEL: a=0,b=1,c=0,sum=1,carry=0
+# KERNEL: a=0,b=1,c=1,sum=0,carry=1
+# KERNEL: a=1,b=0,c=0,sum=1,carry=0
+# KERNEL: a=1,b=0,c=1,sum=0,carry=1
+# KERNEL: a=1,b=1,c=0,sum=0,carry=1
+# KERNEL: a=1,b=1,c=1,sum=1,carry=1
