16756842f46db8680b6878a2a7220d4f09696185
PADALA TEJASAI <padalatejasai@gmail.com>
2026-01-30 05:21:05 -0800

    Create OUTPUT.v

---
+++ DAY_25/OUTPUT.v
@@ -0,0 +1,17 @@
+  0 i=0,op1=xxxxxxxx,op2=xxxxxxxx,comm=xxxx,out=xxxxxxxxxxxxxxxx
+# KERNEL:                    5 i=1,op1=1010,op2=101,comm=0,out=0000000000001111
+# KERNEL:                   10 i=2,op1=1010,op2=101,comm=1,out=0000000000000101
+# KERNEL:                   15 i=3,op1=1010,op2=101,comm=10,out=0000000000001011
+# KERNEL:                   20 i=4,op1=1010,op2=101,comm=11,out=0000000000001001
+# KERNEL:                   25 i=5,op1=1010,op2=101,comm=100,out=0000000000110010
+# KERNEL:                   30 i=6,op1=1010,op2=101,comm=101,out=0000000000000010
+# KERNEL:                   35 i=7,op1=1010,op2=101,comm=110,out=0000000000010100
+# KERNEL:                   40 i=8,op1=1010,op2=101,comm=111,out=0000000000010100
+# KERNEL:                   45 i=9,op1=1010,op2=101,comm=1000,out=0000000000010100
+# KERNEL:                   50 i=10,op1=1010,op2=101,comm=1001,out=0000000000010100
+# KERNEL:                   55 i=11,op1=1010,op2=101,comm=1010,out=0000000000010100
+# KERNEL:                   60 i=12,op1=1010,op2=101,comm=1011,out=0000000000010100
+# KERNEL:                   65 i=13,op1=1010,op2=101,comm=1100,out=0000000000010100
+# KERNEL:                   70 i=14,op1=1010,op2=101,comm=1101,out=0000000000010100
+# KERNEL:                   75 i=15,op1=1010,op2=101,comm=1110,out=0000000000010100
+# KERNEL:                   80 i=16,op1=1010,op2=101,comm=1111,out=0000000000010100
