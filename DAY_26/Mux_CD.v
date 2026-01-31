7c56488eebe385308a1047b463d5a0bc7d2129b6
PADALA TEJASAI <padalatejasai@gmail.com>
2026-01-31 06:38:32 -0800

    Update Mux_CD.v

--- DAY_26/Mux_CD.v
+++ DAY_26/Mux_CD.v
@@ -11,8 +11,8 @@ module mux4_1(in,sel,out);
   input [1:0]sel;
   output out;
   wire[1:0] w;
-  mux (.a(in[1:0]),.s(sel[0]),.out(w[0]));
-  mux (.a(in[3:2]),.s(sel[0]),.out(w[1]));
-  mux (.a(w[1:0]),.s(sel[1]),.out(out));
+  mux u1(.a(in[1:0]),.s(sel[0]),.out(w[0]));
+  mux u2(.a(in[3:2]),.s(sel[0]),.out(w[1]));
+  mux u3(.a(w[1:0]),.s(sel[1]),.out(out));
 endmodule
   
