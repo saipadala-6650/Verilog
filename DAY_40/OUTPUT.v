858f548b9dc765aaf0371a938a45225b3bdd9875
PADALA TEJASAI <padalatejasai@gmail.com>
2026-02-16 03:51:21 -0800

    Create OUTPUT.v

---
+++ DAY_40/OUTPUT.v
@@ -0,0 +1,18 @@
+0 clk=0 sin=x rst=x sout=x
+# KERNEL:                    5 clk=1 sin=x rst=x sout=x
+# KERNEL:                   10 clk=0 sin=0 rst=1 sout=0
+# KERNEL:                   15 clk=1 sin=0 rst=1 sout=0
+# KERNEL:                   20 clk=0 sin=1 rst=0 sout=0
+# KERNEL:                   25 clk=1 sin=1 rst=0 sout=0
+# KERNEL:                   30 clk=0 sin=1 rst=0 sout=0
+# KERNEL:                   35 clk=1 sin=1 rst=0 sout=0
+# KERNEL:                   40 clk=0 sin=1 rst=0 sout=0
+# KERNEL:                   45 clk=1 sin=1 rst=0 sout=0
+# KERNEL:                   50 clk=0 sin=1 rst=0 sout=0
+# KERNEL:                   55 clk=1 sin=1 rst=0 sout=1
+# KERNEL:                   60 clk=0 sin=1 rst=0 sout=1
+# KERNEL:                   65 clk=1 sin=1 rst=0 sout=1
+# KERNEL:                   70 clk=0 sin=1 rst=0 sout=1
+# KERNEL:                   75 clk=1 sin=1 rst=0 sout=1
+# KERNEL:                   80 clk=0 sin=1 rst=0 sout=1
+# KERNEL:                   85 clk=1 sin=1 rst=0 sout=1
