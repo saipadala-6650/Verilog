36e4ff317e8e676c7a05891dbf1118d82addf8a2
PADALA TEJASAI <padalatejasai@gmail.com>
2026-02-21 07:44:02 -0800

    Create OUTPUT.v

---
+++ DAY_45/OUTPUT.v
@@ -0,0 +1,39 @@
+# KERNEL:                    5 clk=1 rst=x sr_in=x sl_in=x mode=x q=x
+# KERNEL:                   15 clk=1 rst=1 sr_in=x sl_in=x mode=x q=0
+# KERNEL:                   25 clk=1 rst=0 sr_in=1 sl_in=0 mode=1 q=0
+# KERNEL:                   35 clk=1 rst=0 sr_in=0 sl_in=0 mode=1 q=0
+# KERNEL:                   45 clk=1 rst=0 sr_in=1 sl_in=0 mode=1 q=0
+# KERNEL:                   55 clk=1 rst=0 sr_in=0 sl_in=0 mode=1 q=0
+# KERNEL:                   65 clk=1 rst=0 sr_in=0 sl_in=0 mode=1 q=1 //<------
+# KERNEL:                   75 clk=1 rst=0 sr_in=0 sl_in=0 mode=1 q=0
+# KERNEL:                   85 clk=1 rst=0 sr_in=0 sl_in=0 mode=1 q=1
+# KERNEL:                   95 clk=1 rst=0 sr_in=0 sl_in=0 mode=1 q=0 //<-----
+# KERNEL:                  105 clk=1 rst=1 sr_in=0 sl_in=0 mode=1 q=0
+# KERNEL:                  115 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=0
+# KERNEL:                  125 clk=1 rst=0 sr_in=0 sl_in=0 mode=0 q=0
+# KERNEL:                  135 clk=1 rst=0 sr_in=0 sl_in=0 mode=0 q=0
+# KERNEL:                  145 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=0
+# KERNEL:                  155 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1 //<====
+# KERNEL:                  165 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=0
+# KERNEL:                  175 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=0
+# KERNEL:                  185 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1 //<====
+# KERNEL:                  195 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  205 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  215 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  225 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  235 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  245 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  255 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  265 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  275 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  285 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  295 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  305 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  315 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  325 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  335 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  345 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  355 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  365 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  375 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  385 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
