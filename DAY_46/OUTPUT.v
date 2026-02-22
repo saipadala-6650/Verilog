2f220d04864c381eade1cf18f13eada470f96072
PADALA TEJASAI <padalatejasai@gmail.com>
2026-02-22 06:52:44 -0800

    Create OUTPUT.v

---
+++ DAY_46/OUTPUT.v
@@ -0,0 +1,26 @@
+# KERNEL:                    5 clk=1 rst=x sr_in=x sl_in=x mode=x q=x
+# KERNEL:                   15 clk=1 rst=1 sr_in=x sl_in=x mode=x q=0
+# KERNEL:                   25 clk=1 rst=0 sr_in=1 sl_in=0 mode=1 q=0
+# KERNEL:                   35 clk=1 rst=0 sr_in=0 sl_in=0 mode=1 q=1
+# KERNEL:                   45 clk=1 rst=0 sr_in=1 sl_in=0 mode=1 q=0
+# KERNEL:                   55 clk=1 rst=0 sr_in=0 sl_in=0 mode=1 q=1
+# KERNEL:                   65 clk=1 rst=1 sr_in=0 sl_in=0 mode=1 q=0
+# KERNEL:                   75 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=0
+# KERNEL:                   85 clk=1 rst=0 sr_in=0 sl_in=0 mode=0 q=1
+# KERNEL:                   95 clk=1 rst=0 sr_in=0 sl_in=0 mode=0 q=0
+# KERNEL:                  105 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=0
+# KERNEL:                  115 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  125 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  135 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  145 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  155 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  165 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  175 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  185 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  195 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  205 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  215 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  225 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  235 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+# KERNEL:                  245 clk=1 rst=0 sr_in=0 sl_in=1 mode=0 q=1
+  
