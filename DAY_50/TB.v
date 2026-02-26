52d730734ca8c6e30cb078bdd9004a03ba1199b7
PADALA TEJASAI <padalatejasai@gmail.com>
2026-02-26 06:56:26 -0800

    Create TB.v

---
+++ DAY_50/TB.v
@@ -0,0 +1,25 @@
+module tb();
+  reg clk,rst;
+  wire [2:0]q;
+  
+  async_down_counter dut(clk,rst,q);
+                 
+  always#5 clk = ~clk;
+  initial
+    begin
+  #10 rst=1; clk=0; //pin=1;
+      #10 rst=0;//pin=1;
+      #100 $finish;
+    end
+                   
+  always@(posedge clk)
+   begin
+     $display($time ," clk=%0b rst=%0b out=%03b",clk,rst,q);
+
+                     end
+  initial
+    begin
+      $dumpfile("dump.vcd");
+      $dumpvars();
+    end
+endmodule
