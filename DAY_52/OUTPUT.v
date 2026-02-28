64de4eb693f1ba0360a6ef7d8b65d41024f17d83
PADALA TEJASAI <padalatejasai@gmail.com>
2026-02-28 04:49:08 -0800

    Create OUTPUT.v

---
+++ DAY_52/OUTPUT.v
@@ -0,0 +1,10 @@
+clk=1 rst=0 q=0000
+# KERNEL: clk=1 rst=0 q=0001
+# KERNEL: clk=1 rst=0 q=0010
+# KERNEL: clk=1 rst=0 q=0011
+# KERNEL: clk=1 rst=0 q=0100
+# KERNEL: clk=1 rst=0 q=0101
+# KERNEL: clk=1 rst=0 q=0110
+# KERNEL: clk=1 rst=0 q=0111
+# KERNEL: clk=1 rst=0 q=1000
+# KERNEL: clk=1 rst=0 q=1001
