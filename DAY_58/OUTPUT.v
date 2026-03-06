a310f7984f986b14730f0ddb48a965963cefb1fe
PADALA TEJASAI <padalatejasai@gmail.com>
2026-03-06 07:10:40 -0800

    Create OUTPUT.v

---
+++ DAY_58/OUTPUT.v
@@ -0,0 +1,11 @@
+Time=5 | q=0001
+# KERNEL: Time=15 | q=0001
+# KERNEL: Time=25 | q=0010
+# KERNEL: Time=35 | q=0100
+# KERNEL: Time=45 | q=1000
+# KERNEL: Time=55 | q=0001
+# KERNEL: Time=65 | q=0010
+# KERNEL: Time=75 | q=0100
+# KERNEL: Time=85 | q=1000
+# KERNEL: Time=95 | q=0001
+# KERNEL: Time=105 | q=0010
