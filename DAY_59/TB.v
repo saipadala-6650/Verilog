ad3ff9aea0c6a2daeae19d8f9d24049b7705ebc0
PADALA TEJASAI <padalatejasai@gmail.com>
2026-03-07 06:05:20 -0800

    Create TB.v

---
+++ DAY_59/TB.v
@@ -0,0 +1,20 @@
+module tb();
+  reg clk,rst;
+  //reg [3:0]Din;
+  wire [3:0]q;
+  
+  
+  Ring_counter dut(.clk(clk),.rst(rst),.q(q));
+  always #5 clk= ~clk;
+  initial
+    begin
+      #10 clk=0; rst=1;
+      
+      #10 rst=0; 
+      #90 $finish;
+    end
+  always@(posedge clk)
+    begin
+    $display($time ," clk=%0b rst=%0b  out=%04b ",clk,rst,q);
+    end
+endmodule
